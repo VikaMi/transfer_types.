8c9c991d6afa2e80e0507cb52637b6b60da96f68
VikaMi <viktoria.mishchanyn@gmail.com>
2016-11-30 17:09:56 +0200

    math functions

--- Main.java
+++ Main.java
@@ -1,23 +1,12 @@
 public class Main {
 
-    static Integer in = 2;
-    static Float fl = 2.6f;
-    static Short s = 3;
-    static Double f = 7.8;
-    static String st = "27";
+    public static void main(String[] args){
+        int a = 4;
+        double b = 5.4;
 
 
-    public static void main(String[] args) {
-        Number n = fl;
-        Number n1 = new Double(2.6);
-        Integer r1 = n.intValue();
-        Integer r2 = n1.intValue();
-
-        Number n3 = f;
-        int ns = n3.intValue();
-
-
-        System.out.println(ns);
+    System.out.println(Math.sin(b) + a);
+    System.out.println(Math.PI);
 
     }
 }
