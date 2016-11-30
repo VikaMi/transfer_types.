9281a24da79d975481544824f5ed871ecdaae87c
VikaMi <viktoria.mishchanyn@gmail.com>
2016-11-30 16:37:00 +0200

    link types

--- Main.java
+++ Main.java
@@ -1,20 +1,22 @@
 public class Main {
 
-    static int a = 2;
-    static double b = 4.5;
-    static char c1 = 100;
+    static Integer in = 2;
+    static Float fl = 2.6f;
+    static Short s = 3;
+    static Double f = 7.8;
+    static String st = "27";
+
 
     public static void main(String[] args) {
-        int c = (int) b;
+        Number n = fl;
+        Number n1 = new Double(2.6);
+        Integer r1 = n.intValue();
+        Integer r2 = n1.intValue();
 
-        double b1 = (double) a;
+        Number n3 = f;
+        int ns = n3.intValue();
 
-        int v = c1;
+        System.out.println(ns);
 
-
-
-        System.out.println(c);
-        System.out.println(b1);
-        System.out.println(c1);
     }
 }
