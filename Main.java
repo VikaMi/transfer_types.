d7c8993e1f82052396e2fe8e75f883eececd7fb8
VikaMi <viktoria.mishchanyn@gmail.com>
2016-11-30 17:28:31 +0200

    ramification. logic type - switch

--- Main.java
+++ Main.java
@@ -1,19 +1,27 @@
 public class Main {
 
     static boolean bool = true;
-    static int a = 9;
+    static int a = 1;
 
     public static void main(String[] args) {
 
-        if (a == 3) {
-            System.out.println("a = 3");
-        } else if (a < 3) {
-            System.out.println("a < 3");
-        } else if (a > 3) {
-            System.out.println("a > 3");
-        }
-    }
+        switch (a) {
 
+            case 5:
+                System.out.println("5");
+                break;
+
+            case 6:
+                System.out.println("6");
+                break;
+
+            case 1:
+                System.out.println("1");
+                break;
+
+        }
+
+    }
 }
 
 
