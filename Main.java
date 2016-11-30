99be0def504d51e4872acb4046c511fd4f8922b8
VikaMi <viktoria.mishchanyn@gmail.com>
2016-11-30 16:11:43 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    static int a = 2;
+    static double b = 4.5;
+    static char c1 = 100;
+
+    public static void main(String[] args) {
+        int c = (int) b;
+
+        double b1 = (double) a;
+
+        int v = c1;
+
+
+
+        System.out.println(c);
+        System.out.println(b1);
+        System.out.println(c1);
+    }
+}
