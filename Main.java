80879a103f9bbcd35869b2b1ce8c00e781b5850e
VikaMi <viktoria.mishchanyn@gmail.com>
2016-12-01 21:41:15 +0200

    massif(string)

--- Main.java
+++ Main.java
@@ -1,23 +1,16 @@
 public class Main {
 
-    static int[] nums = new int[10];
+    static String[] nums = new String[3];
 
     public static void main(String[] args) {
 
-        nums[0] = 2;
-        nums[1] = 5;
-        nums[2] = 3;
-        nums[3] = 26;
-        nums[4] = 23;
-        nums[5] = 11;
-        nums[6] = 64;
-        nums[7] = 62;
-        nums[8] = 92;
-        nums[8] = 26;
-        nums[9] = 1;
+        nums[0] = "Hello";
+        nums[1] = " ";
+        nums[2] = "Vika!";
+
 
         for(int i = 0; i < nums.length; i++){
-            System.out.println(i + " : " + nums[i]);
+            System.out.print(nums[i]);
         }
 
 
