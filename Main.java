be3abb58b4cd62893756bdc0868f8423b91a2453
VikaMi <viktoria.mishchanyn@gmail.com>
2016-12-01 21:23:40 +0200

    recyrsion

--- Main.java
+++ Main.java
@@ -2,14 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int val = calcSum(2, 3);
+        int val = calcSum(-3, 3);
 
         System.out.println(val);
 
     }
-
+//рекурсія - можливість викликати в нашій функції такий самий метод
     static int calcSum(int a, int b){
         int sum = a + b;
+        if(sum == 0) {
+            sum = calcSum(1, 1);
+        }
         return sum;
     }
 }
