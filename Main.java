e0917f16b5caf53034c28547d5285a0010ab4b81
VikaMi <viktoria.mishchanyn@gmail.com>
2016-12-01 18:25:41 +0200

    cycle while

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 10; i > 0; i--) {
+        int i = 10;
+        while (i > 0) {
             System.out.println(i);
+            i--;
 
         }
 
